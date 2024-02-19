bec283d6fc23d74e054aeca10dbae09e22e6f0bd
chonghg22 <64386732+chonghg22@users.noreply.github.com>
2024-02-19 16:25:56 +0900

    Add files via upload
    기본 커밋

---
+++ daeddong-admin/src/main/java/kr/co/daeddongadmin/DaeddongAdminApplication.java
@@ -0,0 +1,15 @@
+package kr.co.daeddongadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class DaeddongAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaeddongAdminApplication.class, args);
+	}
+
+}
---
+++ daeddong-admin/src/main/java/kr/co/daeddongadmin/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package kr.co.daeddongadmin.repository;
+
+import kr.co.daeddongadmin.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
