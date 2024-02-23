92006f5a1ca0c2d8a88caf8c5afa5ddb11f42209
jack <jack@heyratel.com>
2024-02-23 21:05:46 +0900

    CROS 예외 적용

--- daeddong-admin/src/main/java/kr/co/daeddongadmin/DaeddongAdminApplication.java
+++ daeddong-admin/src/main/java/kr/co/daeddongadmin/DaeddongAdminApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@SpringBootApplication
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
@@ -17,5 +20,15 @@ public class DaeddongAdminApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(DaeddongAdminApplication.class);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://ec2-43-201-20-54.ap-northeast-2.compute.amazonaws.com/");
+			}
+		};
 	}
 }
