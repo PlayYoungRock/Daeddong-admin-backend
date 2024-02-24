a34f1a862cc63aee790a59efd7eaf15f8c5b49ac
jack <jack@heyratel.com>
2024-02-24 14:21:09 +0900

    CROS 예외 적용 URL 변경

--- daeddong-admin/src/main/java/kr/co/daeddongadmin/DaeddongAdminApplication.java
+++ daeddong-admin/src/main/java/kr/co/daeddongadmin/DaeddongAdminApplication.java
@@ -27,7 +27,10 @@ public class DaeddongAdminApplication extends SpringBootServletInitializer {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3001");
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3001") // 허용할 오리진(도메인)을 설정합니다.
+						.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드를 설정합니다.
+						.allowedHeaders("*"); // 허용할 HTTP 헤더를 설정합니다.
 			}
 		};
 	}
