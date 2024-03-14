9d1b7114bf639aa5e2d4589b51606c61128ab9e6
jack <jack@heyratel.com>
2024-03-14 18:20:13 +0900

    cors 테스트를 위한 임시 수정

--- daeddong-admin/src/main/java/kr/co/daeddongadmin/DaeddongAdminApplication.java
+++ daeddong-admin/src/main/java/kr/co/daeddongadmin/DaeddongAdminApplication.java
@@ -7,8 +7,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableAspectJAutoProxy
@@ -23,20 +28,36 @@ public class DaeddongAdminApplication extends SpringBootServletInitializer {
 		return builder.sources(DaeddongAdminApplication.class);
 	}
 
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**")
+//						.allowedOrigins("https://localhost:3001", "https://daeddong-admin-dev.web.app")
+//						.allowedMethods("GET", "POST", "PUT", "DELETE")
+//						.allowedHeaders("Authorization", "Content-Type") // 필요한 헤더만 허용하도록 수정
+//						.exposedHeaders("Authorization") // 클라이언트에 노출할 헤더 추가
+//						.allowCredentials(true) // 인증 정보 (쿠키, 인증 헤더 등)을 허용
+//						.maxAge(3600); // preflight 요청 결과를 캐싱할 시간 설정 (초 단위)
+//			}
+//		};
+//	}
+
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("https://localhost:3001", "https://daeddong-admin-dev.web.app")
-						.allowedMethods("GET", "POST", "PUT", "DELETE")
-						.allowedHeaders("Authorization", "Content-Type") // 필요한 헤더만 허용하도록 수정
-						.exposedHeaders("Authorization") // 클라이언트에 노출할 헤더 추가
-						.allowCredentials(true) // 인증 정보 (쿠키, 인증 헤더 등)을 허용
-						.maxAge(3600); // preflight 요청 결과를 캐싱할 시간 설정 (초 단위)
-			}
-		};
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("https://localhost:3001", "https://daeddong-admin-dev.web.app"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		config.setExposedHeaders(Arrays.asList("Authorization"));
+		config.setAllowCredentials(true);
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
 	}
 }
 
